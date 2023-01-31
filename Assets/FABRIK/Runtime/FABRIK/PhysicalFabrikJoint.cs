fa066f98d0c20cdfa13120369b6fdec47655bd65
yohash <jdheebl@gmail.com>
2023-01-31 23:04:23 +1300

    modified physical fabrik scene with some working physical motions. Needs fine-tuning

--- Assets/FABRIK/Runtime/FABRIK/PhysicalFabrikJoint.cs
+++ Assets/FABRIK/Runtime/FABRIK/PhysicalFabrikJoint.cs
@@ -7,40 +7,59 @@ namespace Yohash.FABRIK
   {
     [Header("Declare engine power")]
     [SerializeField] private float Power = 10f;
+    [SerializeField] private float RotationalPower = 50f;
 
     [Header("3-axis PID controllers")]
     [SerializeField] private PIDController controllerX;
     [SerializeField] private PIDController controllerY;
     [SerializeField] private PIDController controllerZ;
 
+    [Header("3-axis rotational PID controller")]
+    [SerializeField] private PidRotationController rotator;
+
     [Header("Final values")]
-    [SerializeField] private Vector3 target;
     [SerializeField] private Vector3 throttle;
+    [SerializeField] private Vector3 torque;
+
+    [Header("Internal vars")]
+    [SerializeField] private Vector3 target;
+    [SerializeField] private Vector3 lookAt;
 
     private Rigidbody rb;
 
-    //public override void AssignPosition(Vector3 position)
-    //{
-    //  target = position;
-    //}
+    public override void AssignPosition(Vector3 position)
+    {
+      target = position;
+    }
 
-    //public override void LookAt(Vector3 lookAtPosition)
-    //{
-    //  // TODO - replace with physical form (add torque)
-    //  transform.LookAt(lookAtPosition);
-    //}
+    public override void LookAt(Vector3 lookAtPosition)
+    {
+      lookAt = lookAtPosition;
+    }
 
     private void FixedUpdate()
     {
       if (rb == null) { rb = GetComponent<Rigidbody>(); }
 
+      float dt = Time.fixedDeltaTime;
+      // solve translational motion
       throttle = new Vector3(
-        controllerX.Update(Time.fixedDeltaTime, rb.position.x, target.x),
-        controllerY.Update(Time.fixedDeltaTime, rb.position.y, target.y),
-        controllerZ.Update(Time.fixedDeltaTime, rb.position.z, target.z)
+        controllerX.Update(dt, rb.position.x, target.x),
+        controllerY.Update(dt, rb.position.y, target.y),
+        controllerZ.Update(dt, rb.position.z, target.z)
       );
 
+      // solve rotational motion
+      torque = rotator.Update(
+        dt,
+        new Pose(transform.position, transform.rotation),
+        lookAt,
+        Vector3.up
+      );
+
+      // apply the forces
       rb.AddForce(throttle * Power);
+      rb.AddRelativeTorque(torque * RotationalPower);
     }
   }
 }
